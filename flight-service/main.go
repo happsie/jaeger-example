545389eb47a01cb93593db238b76fa182e28e88a
Jepzter <njesper98@gmail.com>
2021-06-09 22:35:58 +0200

    Get span reporting working and containerize first application to make sure it works fine in docker-compose

--- flight-service/main.go
+++ flight-service/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 	"github.com/sirupsen/logrus"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
@@ -23,15 +21,14 @@ type Flight struct {
 }
 
 func main() {
-	cfg := &config.Configuration{
+	cfg := config.Configuration{
 		ServiceName: "flight-service",
-		Sampler: &config.SamplerConfig{
-			Type:  "const",
+		Sampler:     &config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
 			Param: 1,
 		},
-		Reporter: &config.ReporterConfig{
-			LogSpans:           true,
-			LocalAgentHostPort: "localhost:16686", // TODO: Change localhost to jaeger
+		Reporter:    &config.ReporterConfig{
+			LogSpans: true,
 		},
 	}
 
@@ -39,15 +36,10 @@ func main() {
 	if err != nil {
 		logrus.Fatalf("could not init tracing: %v\n", err)
 	}
-	defer func() {
-		err := closer.Close()
-		if err != nil {
-			logrus.Error("error closing tracing: %v", err)
-		}
-	}()
 	opentracing.SetGlobalTracer(tracer)
+	defer closer.Close()
 
-	db, err = gorm.Open(mysql.Open("user:password@tcp(localhost:3306)/flight?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{}) // TODO: change localhost to flight-db
+	db, err = gorm.Open(mysql.Open("root:secret@tcp(flight-db:3306)/flights?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		logrus.Fatalf("could not connect to database: %v", err)
 	}
@@ -59,15 +51,10 @@ func main() {
 			c.AbortWithStatus(http.StatusBadRequest)
 			return
 		}
-		spanCtx, _ := tracer.Extract(
-			opentracing.HTTPHeaders,
-			opentracing.HTTPHeadersCarrier(c.Request.Header),
-		)
-		span := tracer.StartSpan("flight-service GET /:id", ext.RPCServerOption(spanCtx))
+		span := tracer.StartSpan("flight-service GET /:id")
 		defer span.Finish()
 
-		ctx := opentracing.ContextWithSpan(c, span)
-		flight := getFlight(ID, ctx)
+		flight := getFlight(ID, span)
 		
 		c.JSON(200, flight)
 	})
@@ -83,8 +70,12 @@ func main() {
 	logrus.Info("server started on port 8080")
 }
 
-func getFlight(ID int, ctx context.Context) Flight {
-	span, _ := opentracing.StartSpanFromContext(ctx, "flight-service MySQL GET Flight")
+func getFlight(ID int, parent opentracing.Span) Flight {
+	tracer := opentracing.GlobalTracer()
+	span := tracer.StartSpan(
+		"flight-service MySQL GET Flight",
+		opentracing.ChildOf(parent.Context()),
+	)
 	defer span.Finish()
 
 	flight := Flight{}
--- readme.md
+++ readme.md
@@ -1,6 +1,14 @@
 ### Jaeger setup example
 
-#### Running
+This example contains two services (flights and passengers) with two different
+mysql instances. 
 
+#### Running
+1. ``git clone https://github.com/Jepzter/jaeger-example.git``
+2. Run ``docker-compose up`` in jaeger-example directory
+3. Make a api call to fetch the flights and it's passenger with 
+```GET http://localhost:8080/api/flight-service/flight-v1/1```
+4. Navigate to to [jaeger ui](http://localhost:16686/search)
+5. Select ``flight-service`` or ``passenger-service`` in the search to see all spans created
 
 #### Tracing
